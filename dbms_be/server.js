dba6f3f81a0797a2b94fc664c59d5a32571e1328
laumgm <lauramarie.manlapaz@tup.edu.ph>
2021-05-26 22:24:54 +0800

    db connection

--- dbms_be/server.js
+++ dbms_be/server.js
@@ -1,10 +1,35 @@
-import express from 'express';
-const app = express();
-app.get('/', (req, res) => {
-    res.send("server is ready");
-});
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import userRoutes from './routes/userRoutes.js'
 
-const port = process.nextTick.PORT || 3000;
+dotenv.config()
+const app = express()
+
+// database connection
+try {
+    const db = await mongoose.connect(process.env.MONGODB_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${db.connection.host}`)
+} catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+}
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/users', userRoutes)
+
+// app.use('/', (req, res) => {
+//     res.send("server is ready");
+// });
+
+const port = process.nextTick.PORT || 5000;
 app.listen(port, () => {
-    console.log("server at http://localhost${port}");
+    console.log(`server running at http://localhost:${port}`);
 })
